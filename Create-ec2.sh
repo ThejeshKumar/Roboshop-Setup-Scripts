c5ad168bfed1ea602b8426068f550983a5423615
mdtk3 <mdtk30.mdtk@gmail.com>
2022-06-01 18:43:11 +0530

    Creating ec2

--- Create-ec2.sh
+++ Create-ec2.sh
@@ -17,4 +17,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
 
-INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=Frontend Name=state,Values=active --output table | grep InstanceId | awk '{print$4}')
+INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId | awk '{print$4}')
