bea6b5c73c67da0f4f2831650fea616c4ea5ea21
mdtk3 <mdtk30.mdtk@gmail.com>
2022-06-01 19:17:21 +0530

    Creating ec2

--- Create-ec2.sh
+++ Create-ec2.sh
@@ -18,3 +18,9 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]" &>/dev/null
 
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId | awk '{print$4}')
+
+IPADDRESS=4(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 |awk '{print$4}')
+
+sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json ./tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z00749231WDS97RWVO261 --change-batch file://sample.json &>/dev/null
+echo DNS Record Created
