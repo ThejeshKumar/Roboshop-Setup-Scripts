3e9ed89ce0b9acf63f1cc3f241cc4864238f3c32
mdtk3 <mdtk30.mdtk@gmail.com>
2022-06-01 17:44:23 +0530

    Creating ec2

--- Createec2.sh
+++ Createec2.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [ "$1" ] ; then
   echo "Instance Name as Argument is Needed"
   exit 1
 fi
- Name=$1
+
+NAME=$1
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print$4}')
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${Name}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${Name}}]"
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${NAME}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${NAME}}]"
