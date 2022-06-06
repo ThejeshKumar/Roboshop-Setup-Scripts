2a96970b8125d33ff2ded8786071081c4c380542
mdtk3 <mdtk30.mdtk@gmail.com>
2022-06-06 10:35:58 +0530

    Creating ec2

--- Create-ec2.sh
+++ Create-ec2.sh
@@ -19,7 +19,7 @@ aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro --instance-m
 
 INSTANCE_ID=$(aws ec2 describe-spot-instance-requests --filters Name=tag:Name,Values=${NAME} Name=state,Values=active --output table | grep InstanceId | awk '{print$4}')
 
-IPADDRESS=aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print$4}'
+IPADDRESS=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --output table | grep PrivateIpAddress | head -n 1 | awk '{print$4}')
 
 sed -e "s/COMPONENT/${NAME}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z00749231WDS97RWVO261 --change-batch file:///tmp/record.json &>/dev/null
